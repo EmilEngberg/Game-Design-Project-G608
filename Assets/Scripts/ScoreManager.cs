811bb09de2f2f8698ed53d6a4238fd625a24ce8b
mfj4000 <mjager20@student.aau.dk>
2023-03-28 15:11:07 +0200

    + scoreboard and timer implementation
    Timer and scoreboard added to project fetched from main

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    
+    public TextMeshProUGUI scoreText;
+
+    int score = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = score.ToString() + " Points";
+    }
+
+    public void AddPoints()
+    {
+        score += 1;
+        scoreText.text = score.ToString() + " Points";
+
+    }
+}
